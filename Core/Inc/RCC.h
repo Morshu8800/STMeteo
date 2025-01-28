03b2d4183ec646af6c35034eb2fa577b7de0939b
FlY DeN <130819892+Morshu8800@users.noreply.github.com>
2025-01-28 10:11:00 +0600

    new_files

---
+++ Core/Inc/RCC.h
@@ -0,0 +1,15 @@
+/*
+ * RCC.h
+ *
+ *  Created on: Dec 18, 2024
+ *      Author: FlY DeN
+ */
+
+#ifndef INC_RCC_H_
+#define INC_RCC_H_
+
+#include "main.h"
+
+void CMSIS_Clock_Config(void);
+
+#endif /* INC_RCC_H_ */
---
+++ Core/Inc/main.h
@@ -0,0 +1,94 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2024 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32g0xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define up_Pin GPIO_PIN_9
+#define up_GPIO_Port GPIOB
+#define RES_Pin GPIO_PIN_1
+#define RES_GPIO_Port GPIOA
+#define DC_Pin GPIO_PIN_3
+#define DC_GPIO_Port GPIOA
+#define CS_Pin GPIO_PIN_4
+#define CS_GPIO_Port GPIOA
+#define LCD_VDD_Pin GPIO_PIN_7
+#define LCD_VDD_GPIO_Port GPIOA
+#define METEO_VDD_Pin GPIO_PIN_0
+#define METEO_VDD_GPIO_Port GPIOB
+#define MEMORY_VDD_Pin GPIO_PIN_1
+#define MEMORY_VDD_GPIO_Port GPIOB
+#define backlight_Pin GPIO_PIN_8
+#define backlight_GPIO_Port GPIOA
+#define speaker_Pin GPIO_PIN_6
+#define speaker_GPIO_Port GPIOC
+#define alarm_disable_Pin GPIO_PIN_3
+#define alarm_disable_GPIO_Port GPIOB
+#define ok_Pin GPIO_PIN_7
+#define ok_GPIO_Port GPIOB
+#define ok_EXTI_IRQn EXTI4_15_IRQn
+#define down_Pin GPIO_PIN_8
+#define down_GPIO_Port GPIOB
+
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
